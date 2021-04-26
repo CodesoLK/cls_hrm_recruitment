6e9de547c9898b0281fc3810a83b155eb5ab54a6
Thisura Weerakkody <thisura@codeso.lk>
2021-04-27 03:46:09 +0530

    -> Bug Fixes

--- models/just_perm.py
+++ models/just_perm.py
@@ -1,49 +1,49 @@
 from flectra import models, fields, api, _
 
-class A1(models.Model):
-    _inherit = 'hr.job'
+# class A1(models.Model):
+#     _inherit = 'hr.job'
 
-class A2(models.Model):
-    _inherit = 'hr.applicant'
+# class A2(models.Model):
+#     _inherit = 'hr.applicant'
 
-class A3(models.Model):
-    _inherit = 'hr.recruitment.stage'
+# class A3(models.Model):
+#     _inherit = 'hr.recruitment.stage'
 
-class A4(models.Model):
-    _inherit = 'hr.applicant.category'
+# class A4(models.Model):
+#     _inherit = 'hr.applicant.category'
 
-class A5(models.Model):
-    _inherit = 'hr.recruitment.degree'
+# class A5(models.Model):
+#     _inherit = 'hr.recruitment.degree'
 
-class A6(models.Model):
-    _inherit = 'hr.recruitment.source'
+# class A6(models.Model):
+#     _inherit = 'hr.recruitment.source'
 
-class A7(models.Model):
-    _inherit = 'hr.department'
+# class A7(models.Model):
+#     _inherit = 'hr.department'
 
-class A8(models.Model):
-    _inherit = 'resource.resource'
+# class A8(models.Model):
+#     _inherit = 'resource.resource'
 
-class A9(models.Model):
-    _inherit = 'hr.work.location'
+# class A9(models.Model):
+#     _inherit = 'hr.work.location'
 
-class A10(models.Model):
-    _inherit = 'hr.employee.line'
+# class A10(models.Model):
+#     _inherit = 'hr.employee.line'
 
-class A11(models.Model):
-    _inherit = 'resource.calendar'
+# class A11(models.Model):
+#     _inherit = 'resource.calendar'
 
-class A12(models.Model):
-    _inherit = 'resource.calendar.attendance'
+# class A12(models.Model):
+#     _inherit = 'resource.calendar.attendance'
 
-class A13(models.Model):
-    _inherit = 'hr.payslip'
+# class A13(models.Model):
+#     _inherit = 'hr.payslip'
 
-class A14(models.Model):
-    _inherit = 'small.furnace'
+# class A14(models.Model):
+#     _inherit = 'small.furnace'
 
-class A15(models.Model):
-    _inherit = 'hr.allowance'
+# class A15(models.Model):
+#     _inherit = 'hr.allowance'
 
-class A15(models.Model):
-    _inherit = 'lunch.taken'
+# class A15(models.Model):
+#     _inherit = 'lunch.taken'
